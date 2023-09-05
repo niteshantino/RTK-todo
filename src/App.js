e3ffe494d88732681c7e37c6267a72ddb32b72ff
nitesh <nitesh.s@antino.com>
2023-09-05 18:54:28 +0530

    UI part done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+// import { useState } from "react";
+import "./App.css";
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todos";
+import { Routes, Route } from "react-router-dom";
+import UpdateTodo from "./components/UpdateTodo";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes >
+      <Route
+        path="/"
+        element={
+          <div className="bg-yellow-50 p-5 mt-10">
+            <AddTodo />
+            <Todos />
+          </div>
+        }
+      />
+      <Route path="/update/:id" element={<UpdateTodo />} />
+    </Routes>
   );
 }
 
